e1b7f3577ee731bc9d2798cf786e3ba95a848991
matspiewak <spiewaak9@gmail.com>
2022-08-20 14:51:39 +0200

    set basic google auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <SessionProvider session={pageProps.session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 }
-
-export default MyApp
