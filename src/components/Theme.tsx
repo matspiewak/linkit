ad602db1fed51dca1422c03ee6ac28a1c22034cc
matspiewak <spiewaak9@gmail.com>
2022-08-31 18:39:42 +0200

    submit style from db to page

---
+++ src/components/Theme.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Theme = ({ bgColor, children }: { bgColor: string, children: JSX.Element[] }) => {
+    console.log(bgColor)
+    return (
+        <StyledTheme color={bgColor}>
+            {children}
+        </StyledTheme>)
+}
+
+const StyledTheme = styled.div`
+width: 100vw;
+height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+background-color: ${props => props.color};
+overflow: hidden;
+`
+
+export default Theme
+
--- src/pages/[user].tsx
+++ src/pages/[user].tsx
@@ -1,32 +1,34 @@
-import { NextPage } from 'next/types';
+import { InferGetStaticPropsType, NextPage } from 'next/types';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import getAllUserUsername from '../db/getAllUserUsernames';
 import { prisma } from '../db/client';
-import { ProfileProps } from '../types/UserContentTypes';
+import { UserProps } from '../types/UserContentTypes';
+import LinkContainer from '../components/LinksContainer';
+import UserContainer from '../components/UserContainer';
+import Toolbar from '../components/Toolbar';
+import Footer from '../components/Footer';
+import Theme from '../components/Theme';
+import Head from 'next/head';
 
-const User: NextPage<ProfileProps> = ({ profile }) => {
-    const userButtons = profile.profile_content.buttons;
+const User: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ page }) => {
     return (
-        <div>
-            <p>{profile.profile_content.page.user}</p>
-            <div>
-                {userButtons.map((button) => {
-                    return (
-                        <button key={button.id}>
-                            <a
-                                href={button.url}
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                {button.text}
-                            </a>
-                        </button>
-                    );
-                })}
-            </div>
-        </div>
+        <>
+            <Head>
+                <title>My page title</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <Theme bgColor={page.content.body.style.backgroundColor}>
+                <Toolbar />
+                <main style={{ width: '100%', height: '100%' }}>
+                    <UserContainer user={page.content.user} />
+                    <LinkContainer links={page.content.links} linkStyle={page.content.body.linkStyle}/>
+                </main>
+                <Footer />
+            </Theme>
+        </>
     );
-};
+}
+
 export const getStaticPaths: GetStaticPaths = async () => {
     const users = await getAllUserUsername();
     const paths = users.map((user) => ({
@@ -39,18 +41,20 @@ export const getStaticPaths: GetStaticPaths = async () => {
     };
 };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getStaticProps: GetStaticProps<UserProps> = async ({ params }) => {
     const user = params!.user as string;
-    const profile = await prisma.profile.findFirst({
+    const res = await prisma.page.findFirst({
         where: {
             user_title: user,
         },
         select: {
-            profile_content: true,
+            id: true,
+            content: true,
         },
-    });
+    })//! fix profile props type
+
     return {
-        props: { profile },
+        props: { page: res },
     };
 };
 
