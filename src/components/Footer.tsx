08d7ac99e5216203c5065b54aff7693cfc1ccc7b
matspiewak <spiewaak9@gmail.com>
2022-08-31 18:37:44 +0200

    init add additional components to user page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Footer = () => {
+
+    return (
+        <BottomBar>
+            footer
+        </BottomBar>
+    )
+}
+
+const BottomBar = styled.footer`
+    margin-bottom: 0
+`
+
+export default Footer
---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const Toolbar = () => {
+
+    return (
+        <Header>
+            {' '}
+        </Header>
+    )
+}
+
+const Header = styled.header`
+    width: 100%;
+    height: 4em;
+`
+
+export default Toolbar
