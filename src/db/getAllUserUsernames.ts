3f576e581f7d4bddb11cade469061f17158b868b
matspiewak <spiewaak9@gmail.com>
2022-08-31 18:40:39 +0200

    adapt getter to new schema

--- src/db/getAllUserUsernames.ts
+++ src/db/getAllUserUsernames.ts
@@ -1,14 +1,27 @@
+import { Prisma } from '@prisma/client';
 import { prisma } from '../db/client';
 
 const getAllUserUsernames = async () => {
-    const users = await prisma.profile.findMany({
-        select: {
-            id: true,
-            user_title: true,
-        },
-    });
+    try {
+        const users = await prisma.page.findMany({
+            select: {
+                id: true,
+                user_title: true,
+            },
+        });
 
-    return users;
+        return users;
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            // The .code property can be accessed in a type-safe manner
+            if (e.code === 'P1001') {
+                console.log(
+                    'There is a unique constraint violation, a new user cannot be created with this email'
+                );
+            }
+        }
+        throw e;
+    }
 };
 
 export default getAllUserUsernames;
