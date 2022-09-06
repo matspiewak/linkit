9f61dd064a65364a4fcdee4f5e668cb49478d285
matspiewak <spiewaak9@gmail.com>
2022-09-06 19:21:17 +0200

    auth options

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth, { Account } from 'next-auth';
+import NextAuth, { Account, NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { prisma } from '../../../db/client';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
@@ -12,74 +12,74 @@ type NewAccount =
       })
     | null;
 
-async function refreshAccessToken(userId: string, newAccount: NewAccount) {
-    try {
-        const userAccount = (await prisma.account.findFirst({
-            where: { userId },
-        })) as NewAccount;
-        if (!userAccount) {
-            return;
-        }
-        if (userAccount.accessTokenExpires) {
-            if (new Date() < userAccount.accessTokenExpires) {
-                return;
-            }
-        }
+//? async function refreshAccessToken(userId: string, newAccount: NewAccount) {
+//     try {
+//         const userAccount = (await prisma.account.findFirst({
+//             where: { userId },
+//         })) as NewAccount;
+//         if (!userAccount) {
+//             return;
+//         }
+//         if (userAccount.accessTokenExpires) {
+//             if (new Date() < userAccount.accessTokenExpires) {
+//                 return;
+//             }
+//         }
 
-        const refreshToken = userAccount.refreshToken;
-        //                      ^?
+//         const refreshToken = userAccount.refreshToken;
+//         //                      ^?
 
-        const url =
-            'https://oauth2.googleapis.com/token?' +
-            new URLSearchParams({
-                client_id: process.env.GOOGLE_CLIENT_ID,
-                client_secret: process.env.GOOGLE_CLIENT_SECRET,
-                grant_type: 'refresh_token',
-                refresh_token: refreshToken,
-            });
+//         const url =
+//             'https://oauth2.googleapis.com/token?' +
+//             new URLSearchParams({
+//                 client_id: process.env.GOOGLE_CLIENT_ID,
+//                 client_secret: process.env.GOOGLE_CLIENT_SECRET,
+//                 grant_type: 'refresh_token',
+//                 refresh_token: refreshToken,
+//             });
 
-        const response = await fetch(url, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            method: 'POST',
-        });
+//         const response = await fetch(url, {
+//             headers: {
+//                 'Content-Type': 'application/x-www-form-urlencoded',
+//             },
+//             method: 'POST',
+//         });
 
-        const refreshedTokens = await response.json();
+//         const refreshedTokens = await response.json();
 
-        if (!response.ok) {
-            if (refreshedTokens.error === 'invalid_grant') {
-                await prisma.account.update({
-                    where: { id: userAccount.id as string },
-                    data: {
-                        refreshToken: newAccount.refreshToken,
-                        accessToken: newAccount!.accessToken,
-                        accessTokenExpires: new Date(
-                            Date.now() + newAccount!.expires_in * 1000
-                        ),
-                    },
-                });
-                return;
-            }
-            throw refreshedTokens;
-        }
+//         if (!response.ok) {
+//             if (refreshedTokens.error === 'invalid_grant') {
+//                 await prisma.account.update({
+//                     where: { id: userAccount.id as string },
+//                     data: {
+//                         refreshToken: newAccount.refreshToken,
+//                         accessToken: newAccount!.accessToken,
+//                         accessTokenExpires: new Date(
+//                             Date.now() + newAccount!.expires_in * 1000
+//                         ),
+//                     },
+//                 });
+//                 return;
+//             }
+//             throw refreshedTokens;
+//         }
 
-        await prisma.account.update({
-            where: { id: userAccount.id as string },
-            data: {
-                refreshToken: refreshedTokens.refresh_token ?? refreshToken,
-                accessToken: refreshedTokens.access_token,
-                accessTokenExpires: new Date(
-                    Date.now() + refreshedTokens.expires_in * 1000
-                ),
-            },
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
+//         await prisma.account.update({
+//             where: { id: userAccount.id as string },
+//             data: {
+//                 refreshToken: refreshedTokens.refresh_token ?? refreshToken,
+//                 accessToken: refreshedTokens.access_token,
+//                 accessTokenExpires: new Date(
+//                     Date.now() + refreshedTokens.expires_in * 1000
+//                 ),
+//             },
+//         });
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
-export default NextAuth({ //! access db's refresh token to use it in a google provider url in order to refresh ~30min session
+export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
@@ -94,19 +94,25 @@ export default NextAuth({ //! access db's refresh token to use it in a google pr
         }),
     ],
     adapter: PrismaAdapter(prisma),
-    events: {
-        async signIn(payload) {
-            await refreshAccessToken(
-                payload.user.id,
-                payload.account as NewAccount
-            );
-        },
-    },
-    // debug: process.env.NODE_ENV === "development",
+    //? events: {
+    //     async signIn(payload) {
+    //         await refreshAccessToken(
+    //             payload.user.id,
+    //             payload.account as NewAccount
+    //         );
+    //     },
+    // },
     secret: process.env.NEXTAUTH_SECRET,
     session: {
         strategy: 'database',
-        updateAge: 0,
         maxAge: 60 * 60 * 24,
     },
-});
+    callbacks: {
+        async session({ session, token, user }) {
+            session.user.id = user.id;
+            return session;
+        },
+    },
+};
+
+export default NextAuth(authOptions);
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,87 @@
-import { NextPage } from 'next/types';
+import { unstable_getServerSession } from 'next-auth';
+import { NextApiRequest, NextApiResponse, NextPage } from 'next/types';
+import { authOptions } from '../api/auth/[...nextauth]';
+import { prisma } from '../../db/client';
+import { IncomingMessage, ServerResponse } from 'http';
+import { ChangeEvent, useState } from 'react';
 
-const Admin: NextPage = () => {
-    return <div>Admin</div>;
+const Admin = ({ session, page }: { session: any, page: any }) => {
+
+        return (
+            <div>
+                <h2>{session.user.email}{"'s dashboard"}</h2>
+                <h2>{page.content.user.title}</h2>
+                <LinkCard />
+            </div>
+        );
 };
+
+interface IState {
+    text: string,
+    url: string
+}
+
+//! fetched sessions requires all cookies, so i have to find some function that requires it and check if i can make it STAHP
+const LinkCard = () => {
+    const [linkInputs, setLinkInputs] = useState<IState>({ text: "", url: "" })
+    const [icon, setIcon] = useState<string>('');
+    const [isVisible, setIsVisible] = useState<boolean>(true);
+    const [position, setPosition] = useState<number>(0);
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const name = e.target.name;
+        setLinkInputs({
+            ...linkInputs,
+            [name]: e.target.value
+        })
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <div>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <label htmlFor='text'>Link text</label>
+                <input type='text' name='text' value={linkInputs.text} onChange={(e) => handleInputChange(e)} />
+                <label htmlFor='url'>Link url</label>
+                <input type='text' name='url' value={linkInputs.url} onChange={(e) => handleInputChange(e)} />
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export async function getServerSideProps(context: { req: (IncomingMessage & { cookies: Partial<{ [key: string]: string; }>; }) | NextApiRequest; res: ServerResponse | NextApiResponse<any>; }) {
+    const session = await unstable_getServerSession(context.req, context.res, authOptions) as any
+
+    if (!session?.user) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
+
+    const page = await prisma.page.findFirst({
+        where: {
+            user_id: session.user.id,
+        },
+        select: {
+            id: true,
+            content: true
+        }
+    })
+
+    return {
+        props: {
+            session,
+            page
+        },
+    }
+}
 
 export default Admin;
