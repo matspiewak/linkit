6ba40024344d512fd570fba505d49ccba926990b
matspiewak <spiewaak9@gmail.com>
2022-09-06 19:20:51 +0200

    refactor types

--- src/types/UserContentTypes.d.ts
+++ src/types/UserContentTypes.d.ts
@@ -1,50 +1,15 @@
-export interface Link {
-    id: string;
-    position: number;
-    text: string;
-    url: string;
-    icon: string;
-}
-
-export interface Body {
-    style: {
-        backgroundColor: string;
-        effect: Effect;
-    };
-    linkStyle: {
-        borderRadius: number;
-        border: Border;
-        backgroundColor: string;
-        shadow: Shadow;
-        font: {
-            family: null | string;
-            size: null | number;
-            color: null | string;
-            bold: number;
-        };
-    };
-}
-
-export interface User {
-    id: string;
-    title: string;
-    profilePicture: string;
-    bio: string | null;
-}
-
-export interface UserData {
-    id: string;
-    content: {
-        body: Body;
-        user: User;
-        links: Array<Link>;
-    };
-}
-
-export interface UserProps {
-    page: UserData;
-}
-
-type Effect = null | 'synthwave';
-type Border = null | '2px black solid';
-type Shadow = null | 'box-shadow: 30px 28px 0px -13px rgba(66, 68, 90, 1)';
+export type Link = Prisma.LinkGetPayload<{ select: { icon: true, order: true, text: true, url: true, id: true } }>
+export type LinkStyle = Prisma.LinkStyleGetPayload<{
+    select: {
+        id: true,
+        background_color: true,
+        border: true,
+        radius: true,
+        shadow: true,
+        font_color: true,
+        font_size: true,
+        font_weight: true,
+        font_family: true,
+        font_style: true,
+    }
+}>
