7bdc11adecd893046e2d7092108904671563ab33
matspiewak <spiewaak9@gmail.com>
2022-09-07 15:30:00 +0200

    add link card, user-link-page iframe

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,87 +1,75 @@
 import { unstable_getServerSession } from 'next-auth';
-import { NextApiRequest, NextApiResponse, NextPage } from 'next/types';
+import { InferGetServerSidePropsType } from 'next/types';
 import { authOptions } from '../api/auth/[...nextauth]';
-import { prisma } from '../../db/client';
-import { IncomingMessage, ServerResponse } from 'http';
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
+import getPageByUserId from '../../db/getPageByUserId';
+import { Link } from '../../types/UserContentTypes';
+import LinkCardContainer from '../../components/LinkCardContainer';
+import styled from '../../styles/dashboard.module.css';
 
-const Admin = ({ session, page }: { session: any, page: any }) => {
+function Dashboard({
+	session,
+	page,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	const [links, setLinks] = useState<Link[]>(page?.Link! || []);
 
-        return (
-            <div>
-                <h2>{session.user.email}{"'s dashboard"}</h2>
-                <h2>{page.content.user.title}</h2>
-                <LinkCard />
-            </div>
-        );
-};
+	if (!page || !page.Profile) {
+		return <div>sds</div>;
+	}
+
+	return (
+		<div className={styled.page}>
+			<nav className={styled.navbar}>
+				<h2>{page.Profile.title}</h2>
+			</nav>
+			<main className={styled.body_container}>
+				<section className={styled.section}>
+					<LinkCardContainer links={links} />
+				</section>
+				<section className={styled.section}>
+					<iframe
+						className={styled.frame}
+						src={page.slug}
+						height='100%'
+						width='95%'
+					/>
+				</section>
+			</main>
+		</div>
+	);
+}
 
 interface IState {
-    text: string,
-    url: string
+	text: string;
+	url: string;
 }
 
 //! fetched sessions requires all cookies, so i have to find some function that requires it and check if i can make it STAHP
-const LinkCard = () => {
-    const [linkInputs, setLinkInputs] = useState<IState>({ text: "", url: "" })
-    const [icon, setIcon] = useState<string>('');
-    const [isVisible, setIsVisible] = useState<boolean>(true);
-    const [position, setPosition] = useState<number>(0);
+export async function getServerSideProps(context: any) {
+	const session = await unstable_getServerSession(
+		context.req,
+		context.res,
+		authOptions
+	);
 
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const name = e.target.name;
-        setLinkInputs({
-            ...linkInputs,
-            [name]: e.target.value
-        })
+	//! commented, because i need type saftety more than i need this for now
+	// if (!session?.user) {
+	//     return {
+	//         redirect: {
+	//             destination: '/',
+	//             permanent: false,
+	//         },
+	//     }
+	// }
 
-    }
+	const page = await getPageByUserId(session?.user.id);
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-    }
-
-    return (
-        <div>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <label htmlFor='text'>Link text</label>
-                <input type='text' name='text' value={linkInputs.text} onChange={(e) => handleInputChange(e)} />
-                <label htmlFor='url'>Link url</label>
-                <input type='text' name='url' value={linkInputs.url} onChange={(e) => handleInputChange(e)} />
-                <button type='submit'>Submit</button>
-            </form>
-        </div>
-    )
+	return {
+		props: {
+			session,
+			page,
+		},
+	};
 }
 
-export async function getServerSideProps(context: { req: (IncomingMessage & { cookies: Partial<{ [key: string]: string; }>; }) | NextApiRequest; res: ServerResponse | NextApiResponse<any>; }) {
-    const session = await unstable_getServerSession(context.req, context.res, authOptions) as any
-
-    if (!session?.user) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
-        }
-    }
-
-    const page = await prisma.page.findFirst({
-        where: {
-            user_id: session.user.id,
-        },
-        select: {
-            id: true,
-            content: true
-        }
-    })
-
-    return {
-        props: {
-            session,
-            page
-        },
-    }
-}
-
-export default Admin;
+export default Dashboard;
