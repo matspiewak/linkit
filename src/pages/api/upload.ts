05fe1356580a41766db4f21b19baa4408867d06e
matspiewak <spiewaak9@gmail.com>
2022-11-09 21:59:23 +0100

    initial implementation of upload service to S3

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,13 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { PutObjectCommand, PutObjectCommandOutput } from '@aws-sdk/client-s3';
+import { s3Client } from '../../libs/s3Client';
 
-type Data = {
-    message: string;
+//?add checker for missing envs
+const params = {
+	Bucket: process.env.AWS_BUCKET_NAME,
+	Key: '',
+	Body: '',
 };
 
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
+const run = async () => {
+	try {
+		const data = await s3Client.send(new PutObjectCommand(params));
+		return data;
+	} catch (err) {
+		throw new Error(getErrorMessage(err));
+	}
+};
+
+function getErrorMessage(error: unknown) {
+	if (error instanceof Error) return error.message;
+	return String(error);
+}
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<PutObjectCommandOutput | { error: string }>
 ) {
-    res.status(200).json({ message: 'Nothing here :(' });
+	run()
+		.then(data => res.status(200).json(data))
+		.catch(err => res.status(500).json({ error: err.message }));
 }
--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -7,6 +7,9 @@ declare global {
 			NEXTAUTH_SECRET: string;
 			MY_SECRET_TOKEN: string;
 			AWS_CLOUDFRONT: string;
+			AWS_ACCESS_KEY_ID: string;
+			AWS_SECRET_ACCESS_KEY: string;
+			AWS_BUCKET_NAME: string;
 			NODE_ENV: 'development' | 'production';
 		}
 	}
